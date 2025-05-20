ef052f731ed7181331b0e773659a3d9ef646c12a
finnnn311 <finnludwagl@gmail.com>
2025-05-20 15:06:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MealOrderApp from "./MealOrderApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MealOrderApp />);
